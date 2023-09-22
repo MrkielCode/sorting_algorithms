b93b9d8676bea963e3f44ec4af2830dd49b57919
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-22 14:15:20 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
+	int temp, swapped_flag = 0;
 	size_t i, j;
 
 	if (array == NULL || size < 2)
@@ -16,6 +16,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped_flag = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -24,7 +25,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+				swapped_flag = 0;
 			}
 		}
+		if (!swapped_flag)
+			break;
 	}
 }
