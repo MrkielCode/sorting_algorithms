812945db353c294b506383bc2e010d0d6a3b9bf7
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-22 14:16:53 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,7 +25,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-				swapped_flag = 0;
+				swapped_flag = 1;
 			}
 		}
 		if (!swapped_flag)
