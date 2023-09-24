a7849a5b35b95852a3ccbcb5d5c2559cf05e5eb1
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-24 15:08:01 +0100

    insertation sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorting a double linked list
+ * using insertation sort
+ * @list: Pointer to the head of the list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *prev, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		prev = current->prev;
+		temp = current;
+
+		while (prev != NULL && prev->n > temp->n)
+		{
+			prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = prev;
+			temp->prev = prev->prev;
+			temp->next = prev;
+
+			if (prev->prev != NULL)
+				prev->prev->next = temp;
+			else
+				*list = temp;
+			prev->prev = temp;
+			prev = temp->prev;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -14,6 +14,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+listint_t *create_listint(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
